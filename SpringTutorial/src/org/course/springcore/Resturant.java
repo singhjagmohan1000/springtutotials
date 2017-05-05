7e0adb04c83e0ce596091aa69f1bec70d04bccfe
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-05-05 02:33:41 -0700

    Adding Setter type DI

--- SpringTutorial/src/org/course/springcore/Resturant.java
+++ SpringTutorial/src/org/course/springcore/Resturant.java
@@ -3,10 +3,16 @@ package org.course.springcore;
 public class Resturant {
 	
 	IHotDrink hotDrink;
-	
-	Resturant(IHotDrink hotDrink){
+	/**
+	 * Constructor Style DI
+	 * @param hotDrink
+	 */
+//	Resturant(IHotDrink hotDrink){
+//		this.hotDrink = hotDrink;
+//		
+//	}
+	public void setHotDrink(IHotDrink hotDrink){
 		this.hotDrink = hotDrink;
-		
 	}
 	public void prepareHotDrink(){
 		hotDrink.prepareHotDrink();
